7faf7eb7e0ab873e2c25af3ccfa4a01cab35aee3
Kafabi Aulia Fasyah <150753433+Kafabi0@users.noreply.github.com>
2025-05-03 22:23:42 +0700

    p

--- snack.js
+++ snack.js
@@ -11,83 +11,94 @@ let snakeBody = [];
 let setIntervalId;
 let score = 0;
 
-
+// Ambil high score dari local storage
 let highScore = localStorage.getItem("Makanan") || 0;
-highScoreElement.innerText = `Makan: ${highScore}`;
+highScoreElement.innerText = `Makan terbanyak: ${highScore}`;
 
-
-
-
+// Atur posisi makanan secara acak
 const updateFoodPosition = () => {
-    foodX = Math.floor(Math.random() * 30) + 1;
-    foodY = Math.floor(Math.random() * 30) + 1;
-}
+  foodX = Math.floor(Math.random() * 30) + 1;
+  foodY = Math.floor(Math.random() * 30) + 1;
+};
 
+// Game over
 const handleGameOver = () => {
-    clearInterval(setIntervalId);
-    alert("Tolol Banget dek ! Ulang Jing,Pukul OK ...");
-    location.reload();
-}
+  clearInterval(setIntervalId);
+  alert("Tolol Banget dek! Ulang Jing, Pukul OK ...");
+  location.reload();
+};
 
+// Ganti arah
+const changeDirection = (e) => {
+  const key = e.key;
+  if (key === "ArrowUp" && velocityY != 1) {
+    velocityX = 0;
+    velocityY = -1;
+  } else if (key === "ArrowDown" && velocityY != -1) {
+    velocityX = 0;
+    velocityY = 1;
+  } else if (key === "ArrowLeft" && velocityX != 1) {
+    velocityX = -1;
+    velocityY = 0;
+  } else if (key === "ArrowRight" && velocityX != -1) {
+    velocityX = 1;
+    velocityY = 0;
+  }
+};
 
-const changeDirection = e => {
-    if (e.key === "ArrowUp" && velocityY != 1) {
-        velocityX = 0;
-        velocityY = -1;
-    } else if (e.key === "ArrowDown" && velocityY != -1) {
-        velocityX = 0;
-        velocityY = 1;
-    } else if (e.key === "ArrowLeft" && velocityX != 1) {
-        velocityX = -1;
-        velocityY = 0;
-    } else if (e.key === "ArrowRight" && velocityX != -1) {
-        velocityX = 1;
-        velocityY = 0;
-    }
-}
+// Deteksi tombol kontrol disentuh
+controls.forEach((button) =>
+  button.addEventListener("click", () =>
+    changeDirection({ key: button.dataset.key })
+  )
+);
 
-
-controls.forEach(button => button.addEventListener("click", () => changeDirection({ key: button.dataset.key })));
-
+// Jalankan game
 const initGame = () => {
-    if (gameOver) return handleGameOver();
-    let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
+  if (gameOver) return handleGameOver();
 
-    if (snakeX === foodX && snakeY === foodY) {
-        updateFoodPosition();
-        snakeBody.push([foodY, foodX]); 
-        score++;
-        highScore = score >= highScore ? score : highScore; 
+  let html = `<div class="food" style="grid-area: ${foodY} / ${foodX}"></div>`;
 
-        localStorage.setItem("Total makan", highScore);
-        scoreElement.innerText = `Score: ${score}`;
-        highScoreElement.innerText = `Makan: ${highScore}`;
+  // Jika makan makanan
+  if (snakeX === foodX && snakeY === foodY) {
+    updateFoodPosition();
+    snakeBody.push([foodY, foodX]);
+    score++;
+    highScore = score >= highScore ? score : highScore;
+    localStorage.setItem("Makanan", highScore);
+    scoreElement.innerText = `Score: ${score}`;
+    highScoreElement.innerText = `Makan: ${highScore}`;
+  }
+
+  // Pergerakan ular
+  snakeX += velocityX;
+  snakeY += velocityY;
+
+  for (let i = snakeBody.length - 1; i > 0; i--) {
+    snakeBody[i] = snakeBody[i - 1];
+  }
+
+  snakeBody[0] = [snakeX, snakeY];
+
+  // Game over jika menabrak dinding
+  if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
+    return (gameOver = true);
+  }
+
+  // Gambar badan ular
+  for (let i = 0; i < snakeBody.length; i++) {
+    html += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
+    if (
+      i !== 0 &&
+      snakeBody[0][1] === snakeBody[i][1] &&
+      snakeBody[0][0] === snakeBody[i][0]
+    ) {
+      gameOver = true;
     }
+  }
 
-    snakeX += velocityX;
-    snakeY += velocityY;
-
-    for (let i = snakeBody.length - 1; i > 0; i--) {
-        snakeBody[i] = snakeBody[i - 1];
-    }
-
-    snakeBody[0] = [snakeX, snakeY];
-
-
-    if (snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30) {
-        return gameOver = true;
-    }
-
-
-    for (let i = 0; i < snakeBody.length; i++) {
-        html += `<div class="head" style="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
-        // Check snake head hit body or no
-        if (i !== 0 && snakeBody[0][1] === snakeBody[i][1] && snakeBody[0][0] === snakeBody[i][0]) {
-            gameOver = true;
-        }
-    }
-    playBoard.innerHTML = html;
-}
+  playBoard.innerHTML = html;
+};
 
 updateFoodPosition();
 setIntervalId = setInterval(initGame, 100);
